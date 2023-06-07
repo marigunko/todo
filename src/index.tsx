340412bbd26039dd47cd561d543a2450f985333c
Marina Gunko <gunko.mail@gmail.com>
2023-06-07 22:40:31 +0300

    4. Маршрутизация React

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,68 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
 import { ToDoListPage } from './pages/ToDoListPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ToDo } from './models/todo-item';
+import { NotFound } from './pages/404';
+import { ItemDescription } from './pages/ItemDescription';
+import { Layout } from './Layouts/Layout';
+
+const todos: ToDo[] = [
+    {
+      id: 0,
+      text: '1 действие',
+      isDone: false    
+    },
+    {
+      id: 1,
+      text: '2 действие',
+      isDone: true   
+    },
+    {
+      id: 2,
+      text: '3 действие',
+      isDone: false    
+    },
+    {
+      id: 3,
+      text: '4 действие',
+      isDone: true    
+    }
+]
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage todos={todos} />
+      },
+      {
+        path: '/todo',
+        element: <ToDoListPage />
+      },
+      {
+        path: '/list/:id',
+        element: <ItemDescription todos={todos} />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+], {basename: '/app/'})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
