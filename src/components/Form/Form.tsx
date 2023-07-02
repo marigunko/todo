93cdc890d7d3212dc880057c371fdc061d705fc8
Marina Gunko <gunko.mail@gmail.com>
2023-07-02 17:43:21 +0300

    6. Styled-components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import './Form.scss'
+import { SetStateAction, useState } from 'react'
+import { FormBlock, FormControl, FormLabel, FormWrapper, FormField } from './Form.styled'
+
+import plusIcon from '../../assets/images/plus.png'
 
 export const Form = (props: { createNewToDo: Function }) => {
     const [text, setText] = useState<string>('')
 
     const formSubmit = (event: React.SyntheticEvent) => {
         event.preventDefault()
+
         if (text) {
             props.createNewToDo(text)
             setText('')
@@ -13,17 +16,17 @@ export const Form = (props: { createNewToDo: Function }) => {
     }
 
     return (
-        <div className="form-wrapper">
-            <form action="#" onSubmit={formSubmit}>
-                <label>
-                    <input 
+        <FormWrapper>
+            <FormBlock action="#" onSubmit={formSubmit}>
+                <FormLabel>
+                    <FormField 
                     value={text} 
                     type="text" 
-                    onChange={(e) =>setText(e.target.value) } 
+                    onChange={(e: { target: { value: SetStateAction<string> } }) => setText(e.target.value)} 
                 />
-                    <button></button>
-                </label>
-            </form>
-        </div>
+                    <FormControl icon={plusIcon} />
+                </FormLabel>
+            </FormBlock>
+        </FormWrapper>
     )
 }
